8b7aef70df959f85f2abe648a4573bbea70977c3
Michael Kenny <57505113+ganymede30@users.noreply.github.com>
2021-03-05 11:53:17 -0500

    Add files via upload

---
+++ MapData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MapData+CoreDataClass.swift
+//  
+//
+//  Created by RaveBizz on 2/17/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MapData)
+public class MapData: NSManagedObject {
+
+}
